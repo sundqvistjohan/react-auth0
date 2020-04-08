2069504d7e56686f214b3e78d296e8db7862322d
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-04-08 19:10:11 +0200

    Change to hard logout

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -48,6 +48,9 @@ export default class Auth {
     localStorage.removeItem("access_token");
     localStorage.removeItem("id_token");
     localStorage.removeItem("expires_at");
-    this.history.push("/");
+    this.auth0.logout({
+      clientID: process.env.REACT_APP_AUTH0_CLIENTID,
+      returnTo: "http://localhost:3000"
+    })
   };
 }
