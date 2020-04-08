00997e7454afec4bcf35613b276b56b117b883f9
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-04-08 14:17:15 +0200

    Adds Callback component and handleAuthentication method

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -1,4 +1,4 @@
-import auth0 from 'auth0-js'
+import auth0 from "auth0-js";
 
 export default class Auth {
   constructor(history) {
@@ -8,11 +8,34 @@ export default class Auth {
       clientID: process.env.REACT_APP_AUTH0_CLIENTID,
       redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
       responseType: "token id_token",
-      scope: "openid profile email"      
-    })
+      scope: "openid profile email",
+    });
   }
 
   login = () => {
     this.auth0.authorize();
-  }
+  };
+
+  handleAuthentication = () => {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        this.setSession(authResult);
+        this.history.push("/");
+      } else if (err) {
+        this.history.push("/");
+        alert(`Error: ${err.error}. Check the console for further details.`);
+        console.log(err);
+      }
+    });
+  };
+
+  setSession = (authResult) => {
+    const expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+
+    localStorage.setItem("access_token", authResult.accessToken);
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", expiresAt);
+  };
 }
