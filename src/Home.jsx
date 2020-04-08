1c27271f1d6fe0f0824c9a6e820173466f9f5a57
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-04-08 14:22:23 +0200

    Adds isAuthenticated method and conditional rendering in Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Home extends Component {
   render() {
+    const { isAuthenticated, login } = this.props.auth;
     return (
       <div>
         <h1>Home</h1>
-        <button onClick={this.props.auth.login}>Log in</button>
+        {isAuthenticated() ? (
+          <Link to="/profile">Profile</Link>
+        ) : (
+          <button onClick={login}>Log in</button>
+        )}
       </div>
     );
   }
