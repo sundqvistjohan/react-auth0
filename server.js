8500fa510191e2692a272bec4979aba057ecd5b5
Johan Sundqvist <sundqvist.johan@outlook.com>
2020-04-09 14:45:34 +0200

    Configures Express to parse JWT

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require("express");
 require("dotenv").config();
+const jwt = require("express-jwt");
+const jwkRsa = require("jwks-rsa");
+
+const checkJwt = jwt({
+  secret: jwkRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`,
+  }),
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}`,
+
+  algorithms: ["RS256"],
+});
 
 const app = express();
 
 app.get("/public", function (req, res) {
   res.json({
     message: "Hello from a public API",
+  });
+});
+
+app.get("/private", checkJwt, function (req, res) {
+  res.json({
+    message: "Hello from a private API",
   });
 });
 
